6e8b889a15bd3281e468c41768fb3771a3182162
Ufere Kalu <dekalusha@gmail.com>
2022-05-12 13:28:07 +0200

    react with context api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import MainNavigation from "./components/Navigation/MainNavigation";
+import AuthPage from "./pages/Auth";
+import BookingsPage from "./pages/Bookings";
+import EventsPage from "./pages/Events";
+import AuthContext from "./context/auth-context";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    token: null,
+    userId: null
+  };
+
+  login = (token, userId, tokenExpiration) => {
+    this.setState({ token: token, userId: userId });
+  };
+
+  logout = () => {
+    this.setState({ token: null, userId: null });
+  };
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Fragment>
+          <AuthContext.Provider
+            value={{
+              token: this.state.token,
+              userId: this.state.userId,
+              login: this.login,
+              logout: this.logout
+            }}
+          >
+            <MainNavigation />
+            <main className="main-content">
+              <Routes>
+                {this.state.token && <Route path="/" element={<Navigate to="/events" replace />} />}
+                {this.state.token && <Route path="/auth" element={<Navigate to="/events" replace />} />}
+                {!this.state.token && <Route path="/auth" element={<AuthPage />} />}
+                <Route path="/events" element={<EventsPage />} />
+                {this.state.token && <Route path="/bookings" element={<BookingsPage />} />}
+                {!this.state.token && <Route path="/" element={<Navigate to="/auth" replace />} />}
+                {!this.state.token && <Route path="/bookings" element={<Navigate to="/auth" replace />} />}
+              </Routes>
+            </main>
+          </AuthContext.Provider>
+        </Fragment>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Spinner.css";
+
+const spinner = () =>
+  <div className="spinner">
+    <div className="lds-spinner">
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>;
+  </div>;
+
+export default spinner;
